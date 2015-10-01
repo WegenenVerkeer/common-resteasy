4d72c9f50b4ab0fe3059c4f37d65cd83b6687292
joachimvda <joachim@progs.be>
2015-10-02 00:25:33 +0200

    fix checkstyle

--- resteasy/src/main/java/be/wegenenverkeer/common/resteasy/exception/ConflictException.java
+++ resteasy/src/main/java/be/wegenenverkeer/common/resteasy/exception/ConflictException.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of wegenenverkeer common-resteasy.
+ * Copyright (c) AWV Agentschap Wegen en Verkeer, Vlaamse Gemeenschap
+ * The program is available in open source according to the Apache License, Version 2.0.
+ * For full licensing details, see LICENSE.txt in the project root.
+ */
+
 package be.wegenenverkeer.common.resteasy.exception;
 
 /**
